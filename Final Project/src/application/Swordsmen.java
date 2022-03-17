50d8b8fbc207b695640fe4f38b88f645772991d0
willi <willi@BY2134M59205546.wit.private>
2022-03-17 15:12:42 -0400

    added shuffle for attacks, action added

--- Final Project/src/application/Swordsmen.java
+++ Final Project/src/application/Swordsmen.java
@@ -1,6 +1,13 @@
 package application;
 
+import java.util.Scanner;
+
+import java.awt.event.KeyAdapter;              
+import java.awt.event.KeyEvent;
+
 public class Swordsmen extends playerChar {
+	
+	
 
 	public Swordsmen() {
 		setName("Swordsmen");
@@ -11,12 +18,115 @@ public class Swordsmen extends playerChar {
 		setKills(0);
 	}
 	
-	public void ultAtk() {
+	public void ultAtk() { // activates ult
 		setElement(5);
 	}
 	
-	public void ultEnd() {
+	public void ultEnd() { // ult ends or battle ends, resets element
 		setElement(0);
 	}
+	
+	public void addKills(int k) { // need this for generation
+		this.kills += k;
+	}
+	
+	public int attack1() { // each return value is just the damage. Dunno what each one will actually do though
+		return 50;
+	}
+	
+	public int attack2() {
+		return 30;
+	}
+	
+	public int attack3() {
+		return 60;
+	}
+	
+	public int attack4() { //this one heals and adds 2 hp for every use. Just so it doesn't become broken.
+		if(tempHp >= hp - 10) {
+			tempHp = hp;
+			hp = hp + 2;
+		}
+		else {
+			tempHp = tempHp + 10;
+			hp = hp + 2;
+		}
+		
+		return 10;
+	}
+	
+	
+	
+	public String action(KeyEvent k) {
+		
+		String act = "";
+		
+		int code = k.getKeyCode();
+		if(code == KeyEvent.VK_LEFT) { // movements
+			act = "LF";
+			return act;
+		}
+		else if(code == KeyEvent.VK_UP) {
+			act = "UP";
+			return act;
+		}
+		else if(code == KeyEvent.VK_RIGHT) {
+			act = "RT";
+			return act;
+		}
+		else if(code == KeyEvent.VK_DOWN) {
+			act = "DN";
+			return act;
+		}
+		else if(code == KeyEvent.VK_X) { // attack
+			for(int i = 0; i < deck.length; i++) {
+				if(i == 3) {
+					if(deck[i] == 1) {
+						Shuffle();
+						return "ATK1";
+					}
+					else if(deck[i] == 2) {
+						Shuffle();
+						return "ATK2";
+					}
+					else if(deck[i] == 3) {
+						Shuffle();
+						return "ATK3";
+					}
+					else if(deck[i] == 4) {
+						Shuffle();
+						return "ATK4";
+					}
+				}
+				if(deck[i] == 1) {
+					deck[i] = 0;
+					return "ATK1";
+				}
+				else if(deck[i] == 2) {
+					deck[i] = 0;
+					return "ATK2";
+				}
+				else if(deck[i] == 3) {
+					deck[i] = 0;
+					return "ATK3";
+				}
+				else if(deck[i] == 4) {
+					deck[i] = 0;
+					return "ATK4";
+				}
+			}
+		}
+		else if(code == KeyEvent.VK_Z) { // base weapon
+			act = "WE";
+			return act;
+		}
+		else if(code == KeyEvent.VK_SPACE) { //ult
+			act = "ULT";
+			return act;
+		}
+		return act;
+	}
+	
+	
 	
 }
--- Final Project/src/application/playerChar.java
+++ Final Project/src/application/playerChar.java
@@ -2,12 +2,18 @@ package application;
 
 public class playerChar {
 	
+	final static double MAX_RNG = 100;
+	final static double MIN_RNG = 0;
+	
+	protected int[] deck = new int[4];
+	
 	protected String characterName; //placeholders for now
 	protected int hp; 
+	protected int tempHp;
 	protected int speed; // 0 is lowest, 10 is highest
 	protected int element; // maybe use an elemental variable type?
 	protected int weaponDmg;
-	private int kills;
+	protected int kills;
 	
 	public playerChar() { //default player model is just the sword user
 		this("Swordsmen", 100, 0, 0, 0, 0);
@@ -16,6 +22,7 @@ public class playerChar {
 	public playerChar(String characterName, int hp, int speed, int element, int weaponDmg, int kills) {
 		this.characterName = characterName;
 		this.hp = hp;
+		this.tempHp = hp;
 		this.speed = speed;
 		this.element = element;
 		this.weaponDmg = weaponDmg;
@@ -68,6 +75,108 @@ public class playerChar {
 	
 	public int getKills() {
 		return kills;
+	}
+	
+	public void Shuffle() { // shuffles attacks
+		double rng = Math.random() * (MAX_RNG - MIN_RNG + 1) + MIN_RNG;
+		if(rng <= 1/24 && rng >= 0) {
+			int[] deck2 = {1, 2, 3, 4};
+			deck = deck2;
+			}
+		else if(rng <= 2/24 && rng > 1/24) {
+			int[] deck2 = {1, 2, 4, 3};
+			deck = deck2;
+		}
+		else if(rng <= 3/24 && rng > 2/24) {
+			int[] deck2 = {1, 4, 2, 3};
+			deck = deck2;
+		}
+		else if(rng <= 4/24 && rng > 3/24) {
+			int[] deck2 = {4, 1, 2, 3};
+			deck = deck2;
+		}
+		else if(rng <= 5/24 && rng > 4/24) {
+			int[] deck2 = {1, 3, 2, 4};
+			deck = deck2;
+		}
+		else if(rng <= 6/24 && rng > 5/24) {
+			int[] deck2 = {1, 3, 4, 2};
+			deck = deck2;
+		}
+		else if(rng <= 7/24 && rng > 6/24) {
+			int[] deck2 = {1, 4, 3, 2};
+			deck = deck2;
+		}
+		else if(rng <= 8/24 && rng > 7/24) {
+			int[] deck2 = {4, 1, 3, 2};
+			deck = deck2;
+		} 
+		else if(rng <= 9/24 && rng > 8/24) {
+			int[] deck2 = {3, 1, 2, 4};
+			deck = deck2;
+		}
+		else if(rng <= 10/24 && rng > 9/24) {
+			int[] deck2 = {3, 1, 4, 2};
+			deck = deck2;
+		}
+		else if(rng <= 11/24 && rng > 10/24) {
+			int[] deck2 = {3, 4, 1, 2};
+			deck = deck2;
+		}
+		else if(rng <= 12/24 && rng > 11/24) {
+			int[] deck2 = {4, 3, 1, 2};
+			deck = deck2;
+		}
+		else if(rng <= 13/24 && rng > 12/24) {
+			int[] deck2 = {2, 1, 3, 4};
+			deck = deck2;
+		}
+		else if(rng <= 14/24 && rng > 13/24) {
+			int[] deck2 = {2, 1, 4, 3};
+			deck = deck2;
+		}
+		else if(rng <= 15/24 && rng > 14/24) {
+			int[] deck2 = {2, 4, 1, 3};
+			deck = deck2;
+		}
+		else if(rng <= 16/24 && rng > 15/24) {
+			int[] deck2 = {4, 2, 1, 3};
+			deck = deck2;
+		}
+		else if(rng <= 17/24 && rng > 16/24) {
+			int[] deck2 = {2, 3, 1, 4};
+			deck = deck2;
+		}
+		else if(rng <= 18/24 && rng > 17/24) {
+			int[] deck2 = {2, 3, 4, 1};
+			deck = deck2;
+		}
+		else if(rng <= 19/24 && rng > 18/24) {
+			int[] deck2 = {2, 4, 3, 1};
+			deck = deck2;
+		}
+		else if(rng <= 20/24 && rng > 19/24) {
+			int[] deck2 = {4, 2, 3, 1};
+			deck = deck2;
+		}
+		else if(rng <= 21/24 && rng > 20/24) {
+			int[] deck2 = {3, 2, 1, 4};
+			deck = deck2;
+		}
+		else if(rng <= 22/24 && rng > 21/24) {
+			int[] deck2 = {3, 2, 4, 1};
+			deck = deck2;
+		}
+		else if(rng <= 23/24 && rng > 22/24) {
+			int[] deck2 = {3, 4, 2, 1};
+			deck = deck2;
+		}
+		else if(rng <= 1 && rng > 23/24) {
+			int[] deck2 = {4, 3, 2, 1};
+			deck = deck2;
+		}
+		
+		
 	}
 }	
 		
