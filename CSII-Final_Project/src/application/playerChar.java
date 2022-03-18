6e879b5d0969d2a624cefc558404c4dd170643b6
fieldd <fieldd@PF2Z32FP.wit.private>
2022-03-18 10:39:38 -0400

    player location

--- CSII-Final_Project/src/application/playerChar.java
+++ CSII-Final_Project/src/application/playerChar.java
@@ -2,111 +2,122 @@ package application;
 
 public class playerChar {
 
-	final static double MAX_RNG = 100;
-	final static double MIN_RNG = 0;
+    final static double MAX_RNG = 100;
+    final static double MIN_RNG = 0;
 
-	protected int[] deck = new int[4];
+    protected int[] deck = new int[4];
 
-	protected String characterName; // placeholders for now
-	protected int hp;
-	protected int tempHp;
-	protected int speed; // 0 is lowest, 10 is highest
-	protected int element; // maybe use an elemental variable type?
-	protected int weaponDmg;
-	protected int kills;
+    protected String characterName; // placeholders for now
+    protected int hp;
+    protected int tempHp;
+    protected int speed; // 0 is lowest, 10 is highest
+    protected int element; // maybe use an elemental variable type?
+    protected int weaponDmg;
+    protected int kills;
+    protected int[] battlelocation = new int[2];
+    
+    public playerChar() { // default player model is just the sword user
+        this("Swordsmen", 100, 0, 0, 0, 0);
+    }
 
-	public playerChar() { // default player model is just the sword user
-		this("Swordsmen", 100, 0, 0, 0, 0);
-	}
+    public playerChar(String characterName, int hp, int speed, int element, int weaponDmg, int kills) {
+        this.characterName = characterName;
+        this.hp = hp;
+        this.tempHp = hp;
+        this.speed = speed;
+        this.element = element;
+        this.weaponDmg = weaponDmg;
+        this.kills = kills;
+        battlelocation[0] = 1;
+        battlelocation[1] = 1;
+    }
 
-	public playerChar(String characterName, int hp, int speed, int element, int weaponDmg, int kills) {
-		this.characterName = characterName;
-		this.hp = hp;
-		this.tempHp = hp;
-		this.speed = speed;
-		this.element = element;
-		this.weaponDmg = weaponDmg;
-		this.kills = kills;
-	}
+    public boolean isDead() {
+        return this.hp <= 0;
+    }
 
-	public boolean isDead() {
-		return this.hp <= 0;
-	}
+    public void setName(String name) {
+        this.characterName = name;
+    }
 
-	public void setName(String name) {
-		this.characterName = name;
-	}
+    public void setHP(int hp) {
+        this.hp = hp;
+    }
 
-	public void setHP(int hp) {
-		this.hp = hp;
-	}
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
+    public void setElement(int element) {
+        this.element = element;
+    }
 
-	public void setElement(int element) {
-		this.element = element;
-	}
+    public void setDMG(int weaponDmg) {
+        this.weaponDmg = weaponDmg;
+    }
 
-	public void setDMG(int weaponDmg) {
-		this.weaponDmg = weaponDmg;
-	}
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+    
+    public void setlocation(int a, int b) {
+        battlelocation[0] = a;
+        battlelocation[1] = b;
+    }
 
-	public void setKills(int kills) {
-		this.kills = kills;
-	}
+    public String getName() {
+        return characterName;
+    }
 
-	public String getName() {
-		return characterName;
-	}
+    public int getHP() {
+        return hp;
+    }
 
-	public int getHP() {
-		return hp;
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public int getSpeed() {
-		return speed;
-	}
+    public int getDMG() {
+        return weaponDmg;
+    }
 
-	public int getDMG() {
-		return weaponDmg;
-	}
+    public int getKills() {
+        return kills;
+    }
+    public int[] getlocation() {
+        return battlelocation;
+    }
 
-	public int getKills() {
-		return kills;
-	}
+    public void Shuffle() { // shuffles attacks
+        double rng = Math.random() * (MAX_RNG - MIN_RNG + 1) + MIN_RNG;
+        if (rng >= 0 && rng < 25) {
+            deck[0] = 1;
+            deck[1] = 2;
+            deck[2] = 3;
+            deck[3] = 4;
+        } else if (rng >= 25 && rng < 50) {
+            deck[0] = 3;
+            deck[1] = 1;
+            deck[2] = 4;
+            deck[3] = 2;
+        } else if (rng >= 50 && rng < 75) {
+            deck[0] = 4;
+            deck[1] = 2;
+            deck[2] = 3;
+            deck[3] = 1;
+        } else if (rng >= 75 && rng <= 100) {
+            deck[0] = 2;
+            deck[1] = 4;
+            deck[2] = 3;
+            deck[3] = 1;
+        }
 
-	public void Shuffle() { // shuffles attacks
-		double rng = Math.random() * (MAX_RNG - MIN_RNG + 1) + MIN_RNG;
-		if (rng >= 0 && rng < 25) {
-			deck[0] = 1;
-			deck[1] = 2;
-			deck[2] = 3;
-			deck[3] = 4;
-		} else if (rng >= 25 && rng < 50) {
-			deck[0] = 3;
-			deck[1] = 1;
-			deck[2] = 4;
-			deck[3] = 2;
-		} else if (rng >= 50 && rng < 75) {
-			deck[0] = 4;
-			deck[1] = 2;
-			deck[2] = 3;
-			deck[3] = 1;
-		} else if (rng >= 75 && rng <= 100) {
-			deck[0] = 2;
-			deck[1] = 4;
-			deck[2] = 3;
-			deck[3] = 1;
-		}
+    }
 
-	}
-
-	public void printDeck() {
-		for (int i = 0; i < deck.length; i++) {
-			System.out.print(deck[i]);
-		}
-		System.out.println("");
-	}
+    public void printDeck() {
+        for (int i = 0; i < deck.length; i++) {
+            System.out.print(deck[i]);
+        }
+        System.out.println("");
+    }
 }
