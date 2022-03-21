0dd53c2838aa226cd17ec653a09ec42f80d6a3c0
willi <willi@BY2134M59205546.wit.private>
2022-03-21 10:10:16 -0400

    added constructor

--- CSII-Final_Project/src/application/singleShotAI.java
+++ CSII-Final_Project/src/application/singleShotAI.java
@@ -1,6 +1,6 @@
 package application;
 
-public class singleShotAI {
+public class singleShotAI extends playerChar{
 
 	final int HP_1 = 50; // HP values for each level of the SinglEShot enemy
 	final int HP_2 = 80;
@@ -11,6 +11,34 @@ public class singleShotAI {
 	final int ATK_2 = 30;
 	final int ATK_3 = 45;
 	final int ATK_MAX = 60;
+	
+	private char type;
+	
+	public singleShotAI() {
+		
+	}
+	
+	public singleShotAI(int lvl) {
+		this.type = 'S';
+		this.element = 0;
+		if(lvl == 1) {
+			this.tempHp =HP_1;
+			this.weaponDmg = ATK_1;
+		}
+		else if(lvl == 2) {
+			this.tempHp =HP_2;
+			this.weaponDmg = ATK_2;
+		}
+		else if(lvl == 3) {
+			this.tempHp =HP_3;
+			this.weaponDmg = ATK_3;
+		}
+		else if(lvl == 4) {
+			this.tempHp =HP_MAX;
+			this.weaponDmg = ATK_MAX;
+		}
+		
+	}
 
 	public boolean snglSht1_Behavior() {
 
