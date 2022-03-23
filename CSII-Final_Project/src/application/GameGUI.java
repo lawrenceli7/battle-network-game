b67618587a338e5791f7ccf03cec7444b769ca7e
Lawrence Li <lil5@wit.edu>
2022-03-22 22:04:04 -0400

    updated the GUI

--- CSII-Final_Project/src/application/GameGUI.java
+++ CSII-Final_Project/src/application/GameGUI.java
@@ -38,14 +38,21 @@ public class GameGUI extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//Starts the scene
 		Scene start = startScreen(primaryStage);
+		
+		//Sets title to the top left corner
 		primaryStage.setTitle("Battle Network Game");
 		primaryStage.setScene(start);
 		primaryStage.show();
 	}
-
+	
+	//Starting screen when launching JavaFX
 	private Scene startScreen(Stage primaryStage) {
+		//Creating Pane to display everything on the screen
 		Pane root = new Pane();
+		
+		//Creating the image and setting the dimensions
 		Image logo = new Image("player.png");
 		ImageView logo1 = new ImageView(logo);
 		logo1.setLayoutX(350);
@@ -55,7 +62,11 @@ public class GameGUI extends Application {
 		logo1.setId("logo");
 		DropShadow ds = new DropShadow(20, Color.BLUE);
 		logo1.setEffect(ds);
+		
+		//Creating a text message
 		Text start = new Text("CLICK TO START GAME!");
+		
+		//Mouse clicking on the image
 		logo1.setOnMouseEntered(event -> {
 			logo1.setLayoutX(350);
 			logo1.setLayoutY(80);
@@ -82,20 +93,26 @@ public class GameGUI extends Application {
 			chooseCharacterScreen(primaryStage);
 		});
 		
+		//Adding background image
 		root.setId("startBackground");
-
+		
+		//Setting dimensions and the scene and starting the scene
 		root.getChildren().addAll(logo1, start);
 		Scene startScene = new Scene(root, 1280, 720);
 		startScene.getStylesheets().add(getClass().getResource("GameGUI.css").toExternalForm());
 		return startScene;
 	}
-
+	
+	//Creating text buttons types for the different player options
 	public StackPane createButtons(String type) {
+		//Creating image, text, and color outline
 		Image button = new Image("playerButton.png", 250, 80, false, false);
 		ImageView iv = new ImageView(button);
 		DropShadow ds = new DropShadow(10, Color.ANTIQUEWHITE);
 		iv.setEffect(ds);
 		Text buttonText = new Text();
+		
+		//Determine text buttons for player type
 		if (type.equals("Swordsmen")) {
 			buttonText.setText(type);
 		} else if (type.equals("Mage")) {
@@ -107,20 +124,25 @@ public class GameGUI extends Application {
 		} else if (type.equals("Martial Artist")) {
 			buttonText.setText(type);
 		}
+		
+		//creating the pane and positioning it to be centered
 		StackPane pane = new StackPane();
 		pane.getChildren().addAll(iv, buttonText);
 		pane.setAlignment(Pos.CENTER);
 		pane.setId("buttonText");
 		return pane;
 	}
-
+	
+	//Screen where you can choose what player to use
 	private void chooseCharacterScreen(Stage primaryStage) {
+		//Displaying text for player option and setting the dimensions onto the screen
 		Text playerOption = new Text();
 		playerOption.setText("CHOOSE PLAYER TYPE");
 		playerOption.setX(600);
 		playerOption.setY(100);
 		playerOption.setId("playerOptionText");
-
+		
+		//Creating the buttons for each player option
 		StackPane SwordPane = createButtons("Swordsmen");
 		StackPane MagePane = createButtons("Mage");
 		StackPane DruidPane = createButtons("Druid");
@@ -133,29 +155,36 @@ public class GameGUI extends Application {
 		setGunner(false);
 		setMartialArtist(false);
 		
+		//Creating event handlers for when each player is clicked
 		eventHandleCharBtns(SwordPane, "Swordsmen", primaryStage);
 		eventHandleCharBtns(MagePane, "Mage", primaryStage);
 		eventHandleCharBtns(DruidPane, "Druid", primaryStage);
 		eventHandleCharBtns(GunnerPane, "Gunner", primaryStage);
 		eventHandleCharBtns(MartialArtistPane, "Martial Artist", primaryStage);
-
+		
+		//Creating the boxes for buttons and positioning it to be centered
+		//Also setting the dimensions
 		VBox btns = new VBox(30);
 		btns.setAlignment(Pos.CENTER);
 		btns.setLayoutX(525);
 		btns.setLayoutY(180);
 		btns.getChildren().addAll(SwordPane, MagePane, DruidPane, GunnerPane, MartialArtistPane);
 		
+		//Adding background image
 		Pane display = new Pane();
 		display.getChildren().addAll(playerOption, btns, charInfo, charInfoTwo);
 		display.setId("startTwoBackground");
 		
+		//Displaying the screen
 		Scene chooseChar = new Scene(display, 1280, 720);
 		chooseChar.getStylesheets().add(getClass().getResource("GameGUI.css").toExternalForm());
 		primaryStage.setScene(chooseChar);
 		primaryStage.show();
 	}
-
+	
+	//When player button is pressed
 	private void eventHandleCharBtns(StackPane pane, String type, Stage primaryStage) {
+		//Determine the character based on mouse click
 		pane.setOnMouseClicked(event -> {
 			if (type.equals("Swordsmen")) {
 				setSwordsmen(true);
