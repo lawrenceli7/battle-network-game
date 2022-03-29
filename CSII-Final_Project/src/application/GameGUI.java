11087bd4cbb5c91e963e89dd9a8705ba6efcd5f3
Lawrence Li <lil5@wit.edu>
2022-03-28 20:13:59 -0400

    added music

--- CSII-Final_Project/src/application/GameGUI.java
+++ CSII-Final_Project/src/application/GameGUI.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
@@ -25,6 +26,8 @@ public class GameGUI extends Application {
 	private boolean isMartialArtist;
 	private VBox charInfo;
 	private VBox charInfoTwo;
+	private MediaPlayer openingMusic;
+	private Music music;
 
 	public GameGUI() {
 		isSwordsmen = false;
@@ -34,25 +37,27 @@ public class GameGUI extends Application {
 		isMartialArtist = false;
 		charInfo = new VBox(15);
 		charInfoTwo = new VBox(15);
+		music = new Music();
+		openingMusic = music.openingMusic();
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Starts the scene
+		// Starts the scene
 		Scene start = startScreen(primaryStage);
-		
-		//Sets title to the top left corner
+
+		// Sets title to the top left corner
 		primaryStage.setTitle("Battle Network Game");
 		primaryStage.setScene(start);
 		primaryStage.show();
 	}
-	
-	//Starting screen when launching JavaFX
+
+	// Starting screen when launching JavaFX
 	private Scene startScreen(Stage primaryStage) {
-		//Creating Pane to display everything on the screen
+		// Creating Pane to display everything on the screen
 		Pane root = new Pane();
-		
-		//Creating the image and setting the dimensions
+
+		// Creating the image and setting the dimensions
 		Image logo = new Image("player.png");
 		ImageView logo1 = new ImageView(logo);
 		logo1.setLayoutX(350);
@@ -62,11 +67,11 @@ public class GameGUI extends Application {
 		logo1.setId("logo");
 		DropShadow ds = new DropShadow(20, Color.BLUE);
 		logo1.setEffect(ds);
-		
-		//Creating a text message
+
+		// Creating a text message
 		Text start = new Text("CLICK TO START GAME!");
-		
-		//Mouse clicking on the image
+
+		// Mouse clicking on the image
 		logo1.setOnMouseEntered(event -> {
 			logo1.setLayoutX(350);
 			logo1.setLayoutY(80);
@@ -93,26 +98,29 @@ public class GameGUI extends Application {
 			chooseCharacterScreen(primaryStage);
 		});
 		
-		//Adding background image
+		//plays music
+		openingMusic.play();
+
+		// Adding background image
 		root.setId("startBackground");
-		
-		//Setting dimensions and the scene and starting the scene
+
+		// Setting dimensions and the scene and starting the scene
 		root.getChildren().addAll(logo1, start);
 		Scene startScene = new Scene(root, 1280, 720);
 		startScene.getStylesheets().add(getClass().getResource("GameGUI.css").toExternalForm());
 		return startScene;
 	}
-	
-	//Creating text buttons types for the different player options
+
+	// Creating text buttons types for the different player options
 	public StackPane createButtons(String type) {
-		//Creating image, text, and color outline
+		// Creating image, text, and color outline
 		Image button = new Image("playerButton.png", 250, 80, false, false);
 		ImageView iv = new ImageView(button);
 		DropShadow ds = new DropShadow(10, Color.ANTIQUEWHITE);
 		iv.setEffect(ds);
 		Text buttonText = new Text();
-		
-		//Determine text buttons for player type
+
+		// Determine text buttons for player type
 		if (type.equals("Swordsmen")) {
 			buttonText.setText(type);
 		} else if (type.equals("Mage")) {
@@ -124,67 +132,67 @@ public class GameGUI extends Application {
 		} else if (type.equals("Martial Artist")) {
 			buttonText.setText(type);
 		}
-		
-		//creating the pane and positioning it to be centered
+
+		// creating the pane and positioning it to be centered
 		StackPane pane = new StackPane();
 		pane.getChildren().addAll(iv, buttonText);
 		pane.setAlignment(Pos.CENTER);
 		pane.setId("buttonText");
 		return pane;
 	}
-	
-	//Screen where you can choose what player to use
+
+	// Screen where you can choose what player to use
 	private void chooseCharacterScreen(Stage primaryStage) {
-		//Displaying text for player option and setting the dimensions onto the screen
+		// Displaying text for player option and setting the dimensions onto the screen
 		Text playerOption = new Text();
 		playerOption.setText("CHOOSE PLAYER TYPE");
 		playerOption.setX(600);
 		playerOption.setY(100);
 		playerOption.setId("playerOptionText");
-		
-		//Creating the buttons for each player option
+
+		// Creating the buttons for each player option
 		StackPane SwordPane = createButtons("Swordsmen");
 		StackPane MagePane = createButtons("Mage");
 		StackPane DruidPane = createButtons("Druid");
 		StackPane GunnerPane = createButtons("Gunner");
 		StackPane MartialArtistPane = createButtons("Martial Artist");
-		
+
 		setSwordsmen(false);
 		setMage(false);
 		setDruid(false);
 		setGunner(false);
 		setMartialArtist(false);
-		
-		//Creating event handlers for when each player is clicked
+
+		// Creating event handlers for when each player is clicked
 		eventHandleCharBtns(SwordPane, "Swordsmen", primaryStage);
 		eventHandleCharBtns(MagePane, "Mage", primaryStage);
 		eventHandleCharBtns(DruidPane, "Druid", primaryStage);
 		eventHandleCharBtns(GunnerPane, "Gunner", primaryStage);
 		eventHandleCharBtns(MartialArtistPane, "Martial Artist", primaryStage);
-		
-		//Creating the boxes for buttons and positioning it to be centered
-		//Also setting the dimensions
+
+		// Creating the boxes for buttons and positioning it to be centered
+		// Also setting the dimensions
 		VBox btns = new VBox(30);
 		btns.setAlignment(Pos.CENTER);
 		btns.setLayoutX(525);
 		btns.setLayoutY(180);
 		btns.getChildren().addAll(SwordPane, MagePane, DruidPane, GunnerPane, MartialArtistPane);
-		
-		//Adding background image
+
+		// Adding background image
 		Pane display = new Pane();
 		display.getChildren().addAll(playerOption, btns, charInfo, charInfoTwo);
 		display.setId("startTwoBackground");
-		
-		//Displaying the screen
+
+		// Displaying the screen
 		Scene chooseChar = new Scene(display, 1280, 720);
 		chooseChar.getStylesheets().add(getClass().getResource("GameGUI.css").toExternalForm());
 		primaryStage.setScene(chooseChar);
 		primaryStage.show();
 	}
-	
-	//When player button is pressed
+
+	// When player button is pressed
 	private void eventHandleCharBtns(StackPane pane, String type, Stage primaryStage) {
-		//Determine the character based on mouse click
+		// Determine the character based on mouse click
 		pane.setOnMouseClicked(event -> {
 			if (type.equals("Swordsmen")) {
 				setSwordsmen(true);
@@ -198,8 +206,8 @@ public class GameGUI extends Application {
 				setMartialArtist(true);
 			}
 		});
-		
-		pane.setOnMouseEntered(event->{
+
+		pane.setOnMouseEntered(event -> {
 			pane.getChildren().remove(1);
 			Text btnText = new Text(type);
 			btnText.setId("whiteBtnText");
@@ -208,8 +216,8 @@ public class GameGUI extends Application {
 			charInfo.setVisible(true);
 			charInfoTwo.setVisible(true);
 		});
-		
-		pane.setOnMouseExited(event->{
+
+		pane.setOnMouseExited(event -> {
 			pane.getChildren().remove(1);
 			Text btnText = new Text(type);
 			btnText.setId("btnText");
@@ -217,19 +225,18 @@ public class GameGUI extends Application {
 			charInfo.setVisible(false);
 			charInfoTwo.setVisible(false);
 		});
-		
+
 	}
 
 	private VBox charInfoBox(String type) {
 		return charInfo;
-	
-		
+
 	}
 
 	private void run(GraphicsContext gc) {
 
 	}
-	
+
 	public boolean isSwordsmen() {
 		return isSwordsmen;
 	}
---
+++ CSII-Final_Project/src/application/Music.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class Music {
+	private MediaPlayer mediaPlayer;
+
+	private MediaPlayer createSound(String musicFile, double volume) {
+		Media sound = new Media(new File(musicFile).toURI().toString());
+		mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.setVolume(volume);
+		return mediaPlayer;
+
+	}
+	
+	public MediaPlayer openingMusic() {
+		return createSound("./src/intro.mp3", 0.5);
+	}
+
+}
